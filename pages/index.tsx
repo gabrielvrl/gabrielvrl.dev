7472c7222c5435efa60941b17b62636750c39e59
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-10-09 12:25:38 -0300

    Fixing images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 /* import Image from 'next/image'*/
+import { prefix } from '../utils/prefix';
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -61,6 +62,7 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
+            <img src={`${prefix}/vercel.svg`} alt="Vercel Logo" width={72} height={16}/>
             {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
           </span>
         </a>
